1d9b9a83526b598ce86a76cc93b9d2ae46796bea
limon100 <mahmudurlimon41@gmail.com>
2021-08-04 22:46:47 +0600

    update main

--- Advanced Road Lane Finding/CameraCalibration.py
+++ Advanced Road Lane Finding/CameraCalibration.py
@@ -5,20 +5,9 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 class CameraCalibration():
-    """ Class that calibrate camera using chessboard images.
-
-    Attributes:
-        mtx (np.array): Camera matrix 
-        dist (np.array): Distortion coefficients
-    """
+    
     def __init__(self, image_dir, nx, ny, debug=False):
-        """ Init CameraCalibration.
-
-        Parameters:
-            image_dir (str): path to folder contains chessboard images
-            nx (int): width of chessboard (number of squares)
-            ny (int): height of chessboard (number of squares)
-        """
+        
         fnames = glob.glob("{}/*".format(image_dir))
         objpoints = []
         imgpoints = []
@@ -29,13 +18,13 @@ class CameraCalibration():
         
         # Go through all chessboard images
         for f in fnames:
-            img = mpimg.imread(f)
 
-            # Convert to grayscale image
+            img = mpimg.imread(f)
             gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
             # Find chessboard corners
             ret, corners = cv2.findChessboardCorners(img, (nx, ny))
+
             if ret:
                 imgpoints.append(corners)
                 objpoints.append(objp)
@@ -47,14 +36,7 @@ class CameraCalibration():
             raise Exception("Unable to calibrate camera")
 
     def undistort(self, img):
-        """ Return undistort image.
-
-        Parameters:
-            img (np.array): Input image
-
-        Returns:
-            Image (np.array): Undistorted image
-        """
+        
         # Convert to grayscale image
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
